d6a23e3650e2c6f0994eb988a73500f77e01328b
Aditya <99547519+CunninglyPro@users.noreply.github.com>
2022-12-24 16:46:05 +0530

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -2,5 +2,5 @@ function addUser() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("login_details", user_name);
 
-    window.location = "kwitter_room.html"
+    window.location = "kwitter_room.html";
 }
